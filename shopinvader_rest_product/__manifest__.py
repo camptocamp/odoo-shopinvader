c1a52108c9f1f6b106841effcdf0e937576248bc
Simone Orsi <simahawk@gmail.com>
2019-08-28 18:23:51 +0200

    Draft shopinvader_rest_product

---
+++ shopinvader_rest_product/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Shopinvader REST product",
+    "version": "12.0.1.0.0",
+    "author": "Camptocamp",
+    "website": "www.camptocamp.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": ["shopinvader", "base_jsonify"],
+    "installable": True,
+}

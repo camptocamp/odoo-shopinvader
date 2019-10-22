3fd997b86c8f8a48841aefdc19105931e939e4b0
Simone Orsi <simahawk@gmail.com>
2019-10-22 16:27:13 +0200

    Add shopinvader_customer_multi_user

---
+++ shopinvader_customer_multi_user/components/access_info.py
@@ -0,0 +1,33 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class PartnerAccess(Component):
+    _inherit = "shopinvader.partner.access"
+
+    def profile(self, partner=None):
+        info = super().profile(partner=partner)
+        partner = partner or self.partner
+        if partner != self.partner_user:
+            info.update({"update": False, "readonly": True})
+        return info
+
+    def address(self, address_id):
+        info = super().address(address_id)
+        if (
+            self.partner != self.partner_user
+            and not address_id == self.partner_user
+        ):
+            info.update(
+                {
+                    "read": True,
+                    "update": False,
+                    "delete": False,
+                    # easy check on client side for being able to edit
+                    "readonly": False,
+                }
+            )
+        return info

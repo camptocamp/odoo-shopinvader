e861ad273e7d8c5a86a0a81dd8d82ef80c2d2f17
Simone Orsi <simahawk@gmail.com>
2019-11-04 13:54:28 +0100

    fixup! Validation: refactor w/ partner.validator component

--- shopinvader/models/shopinvader_backend.py
+++ shopinvader/models/shopinvader_backend.py
@@ -11,7 +11,7 @@ from odoo.http import request
 
 class ShopinvaderBackend(models.Model):
     _name = "shopinvader.backend"
-    _inherit = ["collection.base"]
+    _inherit = "connector.backend"
     _description = "Shopinvader Backend"
 
     name = fields.Char(required=True)

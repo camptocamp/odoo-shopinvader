961f590d95963a0637a271dc7952cbcb9b3e7a6a
Simone Orsi <simahawk@gmail.com>
2019-11-06 12:30:37 +0100

    [12.0] Customer: export and accept partner industry

--- shopinvader/services/address.py
+++ shopinvader/services/address.py
@@ -106,6 +106,18 @@ class AddressService(Component):
                     }
                 },
             },
+            "industry_id": {
+                "required": False,
+                "type": "dict",
+                "schema": {
+                    "id": {
+                        "coerce": to_int,
+                        "required": False,
+                        "nullable": True,
+                        "type": "integer",
+                    }
+                },
+            },
             "is_company": {"coerce": to_bool, "type": "boolean"},
             "opt_in": {"coerce": to_bool, "type": "boolean"},
             "opt_out": {"coerce": to_bool, "type": "boolean"},
@@ -144,6 +156,7 @@ class AddressService(Component):
             "address_type",
             "is_company",
             ("title", ["id", "name"]),
+            ("industry_id", ["id", "name"]),
         ]
         return res
 

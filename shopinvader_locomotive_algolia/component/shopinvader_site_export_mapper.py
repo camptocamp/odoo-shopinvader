19a809e63e8da17926ed190fc50531a7ad1c22c1
Simone Orsi <simahawk@gmail.com>
2019-11-09 11:48:38 +0100

    [12.0] Add shopinvader_locomotive_algolia

---
+++ shopinvader_locomotive_algolia/component/shopinvader_site_export_mapper.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class ShopinvaderSiteExportMapper(Component):
+    _inherit = ["shopinvader.site.export.mapper"]
+
+    # matching $searchengine_name + '_config'
+    def _algolia_config(self, se_backend, config):
+        if (
+            self.options["force"]
+            or not config.get("application_id")
+            or not config.get("api_key")
+        ):
+            config.update(
+                {
+                    "application_id": se_backend.specific_backend.algolia_app_id,
+                    "api_key": se_backend.specific_backend.algolia_api_key,
+                }
+            )
+        return config

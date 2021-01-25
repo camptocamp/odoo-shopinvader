fc9b814252c0cd92b63ccdf4aa58aa7ae452d832
Simone Orsi <simahawk@gmail.com>
2021-01-25 13:54:54 +0100

    shopinvader: refactor partner validation
    Customers (bindings) now have 3 states: pending, active, inactive.
    Addresses keep the flag only as they are not bound to a backend.
    For both of them a wizard is provided to properly change their status
    and get users notified.

--- shopinvader/__manifest__.py
+++ shopinvader/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader",
     "summary": "Shopinvader",
-    "version": "13.0.4.2.2",
+    "version": "13.0.4.3.0",
     "category": "e-commerce",
     "website": "https://shopinvader.com",
     "author": "Akretion",
@@ -40,6 +40,8 @@
         "wizards/shopinvader_category_binding_wizard.xml",
         "wizards/shopinvader_category_unbinding_wizard.xml",
         "wizards/shopinvader_partner_binding.xml",
+        "wizards/shopinvader_address_validate.xml",
+        "wizards/shopinvader_partner_validate.xml",
         "views/shopinvader_menu.xml",
         "views/shopinvader_product_view.xml",
         "views/shopinvader_variant_view.xml",

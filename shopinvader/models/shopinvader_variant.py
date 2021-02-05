be78b5c245480b12af8a05a18daa5f3d3a44df42
Simone Orsi <simahawk@gmail.com>
2021-02-05 10:52:25 +0100

    shopinvader.variant: boost _compute_main_product
    Avoid to load template m2m to variants (quite slow)
    and avoid too many joined queries.

--- shopinvader/models/shopinvader_variant.py
+++ shopinvader/models/shopinvader_variant.py
@@ -2,6 +2,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from contextlib import contextmanager
+from itertools import groupby
 
 from odoo import api, fields, models
 from odoo.tools import float_compare, float_round
@@ -242,11 +243,34 @@ class ShopinvaderVariant(models.Model):
         return res
 
     def _compute_main_product(self):
+        # Respect same order.
+        order_by = self.env["product.product"]._order.split(",")
+        fields_to_read = ["tmpl_record_id"] + order_by
+        tmpl_ids = self.mapped("tmpl_record_id").ids
+        # Use sudo to bypass permissions (we don't care)
+        _variants = self.sudo().search(
+            [("tmpl_record_id", "in", tmpl_ids)], order="tmpl_record_id"
+        )
+        # Use `load=False` to not load template name
+        variants = _variants.read(fields_to_read, load=False)
+        var_by_tmpl = groupby(variants, lambda x: x["tmpl_record_id"])
+
+        def pick_1st_variant(prods):
+            # NOTE: if the order is changed by adding `asc/desc` this can be broken
+            # but it's very unlikely that the default order for product.product
+            # will be changed.
+            return (
+                fields.first(
+                    sorted(prods, key=lambda var: [var[x] for x in order_by])
+                )
+                or {}
+            )
+
+        main_by_tmpl = {
+            tmpl: pick_1st_variant(prods).get("id")
+            for tmpl, prods in var_by_tmpl
+        }
         for record in self:
-            if (
-                record.record_id
-                == record.product_tmpl_id.product_variant_ids[0]
-            ):
-                record.main = True
-            else:
-                record.main = False
+            record.main = (
+                main_by_tmpl.get(record.tmpl_record_id.id) == record.id
+            )

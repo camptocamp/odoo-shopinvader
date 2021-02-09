5b50c0e3bc190ec52b0b040a55980d7c34cae61c
Simone Orsi <simahawk@gmail.com>
2021-02-09 12:37:05 +0100

    fixup! fixup! shopinvader: refactor partner validation

--- shopinvader/models/res_partner.py
+++ shopinvader/models/res_partner.py
@@ -62,6 +62,16 @@ class ResPartner(models.Model):
         compute_sudo=True,
         store=True,
     )
+    has_shopinvader_address_to_validate = fields.Boolean(
+        help="This partner has at least an address to validate for Shopinvader.",
+        compute="_compute_has_shopinvader_address_to_validate",
+        compute_sudo=True,
+    )
+    display_validate_address = fields.Boolean(
+        help="Tech field to be used in views to display/hide validate action.",
+        compute="_compute_display_flags",
+        compute_sudo=True,
+    )
 
     @api.depends("is_blacklisted")
     def _compute_opt_in(self):
@@ -97,6 +107,56 @@ class ResPartner(models.Model):
                     lambda x: x.state == STATE_PENDING
                 )
             )
+
+    @api.depends(
+        "has_shopinvader_user_active",
+        "child_ids.address_type",
+        "child_ids.is_shopinvader_active",
+    )
+    def _compute_has_shopinvader_address_to_validate(self):
+        addresses_to_validate = self.read_group(
+            [
+                ("parent_id", "child_of", self.ids),
+                ("parent_id.has_shopinvader_user_active", "=", True),
+                ("shopinvader_bind_ids", "=", False),
+                ("address_type", "=", "address"),
+                ("is_shopinvader_active", "=", False),
+            ],
+            ["parent_id"],
+            ["parent_id"],
+        )
+        by_parent = {
+            x["parent_id"][0]: x["parent_id_count"]
+            for x in addresses_to_validate
+            if x["parent_id"]
+        }
+        for record in self:
+            record.has_shopinvader_address_to_validate = bool(
+                by_parent.get(record.id)
+            )
+
+    def _compute_display_flags(self):
+        for record in self:
+            record.display_validate_address = (
+                record._display_validate_address()
+            )
+
+    def _display_validate_address(self):
+        if self.has_shopinvader_user:
+            # this is a user, no address
+            return False
+        has_parent = self._get_has_shopinvader_parent()
+        return has_parent and self.address_type == "address"
+
+    def _get_has_shopinvader_parent(self):
+        has_shopinvader_user = self.parent_has_shopinvader_user
+        if has_shopinvader_user:
+            return True
+        parent = self.parent_id
+        while parent and not has_shopinvader_user:
+            parent = parent.parent_id
+            has_shopinvader_user = parent.parent_has_shopinvader_user
+        return has_shopinvader_user
 
     @api.depends("parent_id")
     def _compute_address_type(self):
@@ -179,7 +239,16 @@ class ResPartner(models.Model):
         return action_data
 
     def _get_shopinvader_validate_address_wizard(self, **kw):
-        vals = dict(partner_ids=self.ids, **kw)
+        ids = self.ids
+        if self.env.context.get("validate_all"):
+            ids = self.child_ids.filtered_domain(
+                [
+                    ("has_shopinvader_user", "=", False),
+                    ("address_type", "=", "address"),
+                    ("is_shopinvader_active", "=", False),
+                ]
+            ).ids
+        vals = dict(partner_ids=ids, **kw)
         return self.env["shopinvader.address.validate"].create(vals)
 
     def get_shop_partner(self, backend):

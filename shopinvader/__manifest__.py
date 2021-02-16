0429674bcb97c48648099a61c6cbb3eb45db33f2
Simone Orsi <simahawk@gmail.com>
2021-02-16 14:00:54 +0100

    fixup! fixup! fixup! fixup! shopinvader: refactor partner validation

--- shopinvader/__manifest__.py
+++ shopinvader/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader",
     "summary": "Shopinvader",
-    "version": "13.0.5.2.1",
+    "version": "13.0.5.3.0",
     "category": "e-commerce",
     "website": "https://shopinvader.com",
     "author": "Akretion",

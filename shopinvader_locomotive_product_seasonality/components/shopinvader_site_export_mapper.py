290e442c5d3fc41df3f258515f777ec2dc065ce7
Simone Orsi <simahawk@gmail.com>
2021-06-30 10:49:38 +0200

    fixup! Add shopinvader_locomotive_product_seasonality

--- shopinvader_locomotive_product_seasonality/components/shopinvader_site_export_mapper.py
+++ shopinvader_locomotive_product_seasonality/components/shopinvader_site_export_mapper.py
@@ -5,10 +5,17 @@
 from odoo.fields import first
 
 from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import mapping
 
 
 class ShopinvaderSiteExportMapper(Component):
     _inherit = ["shopinvader.site.export.mapper"]
+
+    @mapping
+    def default_seasonal_config(self, record):
+        return {
+            "default_seasonal_config_id": record.company_id.default_seasonal_config_id.id
+        }
 
     def _get_indexes_config(self, se_backend):
         indices, routes = super()._get_indexes_config(se_backend)

baedbee43f582933f05fb085839ec14b277ffb87
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-07 11:37:21 -0300

    [MIG] shopinvader_partner_firstname: Migration to 14.0

--- shopinvader_partner_firstname/tests/test_firstname.py
+++ shopinvader_partner_firstname/tests/test_firstname.py
@@ -1,25 +1,57 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
+# Copyright 2021 Camptocamp (http://www.camptocamp.com)
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.exceptions import UserError
 
 from odoo.addons.shopinvader.tests.test_customer import TestCustomerCommon
 
 
 class TestCustomer(TestCustomerCommon):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+    def setUp(self, *args, **kwargs):
+        super().setUp(*args, **kwargs)
+        # TODO: This should be in setUpClass, but it should be changed
+        # in shopinvader TestCustomerCommon
+        self.data.update(
+            {
+                "external_id": "jdoe",
+                "name": "John Doe",
+                "firstname": "John",
+                "lastname": "Doe",
+            }
+        )
 
     def test_create_customer_firstname(self):
-        data = dict(
-            self.data,
-            external_id="jdoe",
-            email="john@doe.com",
-            firstname="John",
-            lastname="Doe",
-        )
-        params = data.copy()
-        res = self.service.dispatch("create", params=params)["data"]
-        partner = self.env["res.partner"].browse(res["id"])
-        data["name"] = "John Doe"
-        self._test_partner_data(partner, data)
+        """Test that we can create partners using firstname and lastname"""
+        params = self.data.copy()
+        params.pop("name")
+        partner_id = self.service.dispatch("create", params=params)["data"]["id"]
+        partner = self.env["res.partner"].browse(partner_id)
+        self._test_partner_data(partner, self.data)
+
+    def test_create_customer_normal(self):
+        """Test that we can create partners using name"""
+        params = self.data.copy()
+        params.pop("firstname")
+        params.pop("lastname")
+        partner_id = self.service.dispatch("create", params=params)["data"]["id"]
+        partner = self.env["res.partner"].browse(partner_id)
+        self._test_partner_data(partner, self.data)
+
+    def test_create_customer_exclude(self):
+        """Test that we can't use name with firstname and lastname"""
+        # Case 1: name, firstname and lastname can't all be together
+        params = self.data.copy()
+        with self.assertRaises(UserError):
+            self.service.dispatch("create", params=params)
+        # Case 2: name and firstname can't be together
+        params = self.data.copy()
+        params.pop("lastname")
+        with self.assertRaises(UserError):
+            self.service.dispatch("create", params=params)
+        # Case 3: name and lastname can't be together
+        params = self.data.copy()
+        params.pop("firstname")
+        with self.assertRaises(UserError):
+            self.service.dispatch("create", params=params)

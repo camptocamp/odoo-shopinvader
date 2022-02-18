ae6f7ea37fe4f240bccf32cb3a08810c7cfc1dd4
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2022-02-18 14:00:24 +0100

    [ADD] shopinvader_data_stored

---
+++ shopinvader_data_stored/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Shopinvader Data Stored",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp",
+    "development_status": "Production/Stable",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": [
+        "shopinvader",
+        "jsonify_stored",
+    ],
+    "data": [
+        "views/shopinvader_variant_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ shopinvader_data_stored/models/shopinvader_variant.py
@@ -0,0 +1,15 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ShopinvaderVariant(models.Model):
+    _inherit = ["shopinvader.variant", "jsonify.stored.mixin"]
+    _name = "shopinvader.variant"
+    _description = "Shopinvader Variant"
+
+    def _get_shop_data(self):
+        """Use pre-computed index data."""
+        return self.jsonified_data

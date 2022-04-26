1dbc72812303d8575c889adfd4f90f09bff096be
Simone Orsi <simahawk@gmail.com>
2022-04-26 11:23:31 +0200

    fixup! fixup! shopinvader_data_stored: Add pre_init_hook script

--- shopinvader_data_stored/hooks.py
+++ shopinvader_data_stored/hooks.py
@@ -1,9 +1,9 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.jsonifier_stored.hooks import add_jsonify_column
+from odoo.addons.jsonifier_stored.hooks import add_jsonifier_column
 
 
 def pre_init_hook(cr):
     table_name = "shopinvader_variant"
-    add_jsonify_column(cr, table_name)
+    add_jsonifier_column(cr, table_name)

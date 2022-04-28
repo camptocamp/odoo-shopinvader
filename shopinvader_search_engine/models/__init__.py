990b8e0f1a914b5050438953a6b2a94c739cc544
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-04-28 16:54:01 +0300

    [IMP] add shared backend

--- shopinvader_search_engine/models/__init__.py
+++ shopinvader_search_engine/models/__init__.py
@@ -2,4 +2,5 @@ from . import shopinvader_backend
 from . import shopinvader_se_binding
 from . import shopinvader_variant
 from . import shopinvader_category
+from . import se_backend
 from . import se_index

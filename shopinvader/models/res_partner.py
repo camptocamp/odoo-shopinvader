5a35bf983b46620d2e942f68fe61f33c0e2f1a46
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-05-31 11:21:00 +0300

    fixup! [14.0][IMP] allow copy partners unrelated to shopinvader

--- shopinvader/models/res_partner.py
+++ shopinvader/models/res_partner.py
@@ -73,8 +73,13 @@ class ResPartner(models.Model):
                     res_partner rp
                 WHERE rp.email is not null
                     and rp.active = True
-                    and EXISTS (SELECT FROM shopinvader_partner sp WHERE sp.record_id = rp.id)
+                    and EXISTS (
+                        SELECT FROM shopinvader_partner sp
+                        WHERE sp.record_id = rp.id
+                    )
+                    and rp.is_shopinvader_active
                 ) dups
+
             WHERE dups.Row > 1;
         """
         )

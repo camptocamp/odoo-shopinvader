9d7337ff83cbeb0be4968f9ffa9390a9a5997716
Simone Orsi <simahawk@gmail.com>
2022-08-25 14:31:41 +0200

    s_customer_validate: improve address wizard
    * select only one backend at time
    * show a warning if mixed backends are involved
    * add some tests

--- shopinvader_customer_validate/wizards/shopinvader_address_validate.py
+++ shopinvader_customer_validate/wizards/shopinvader_address_validate.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Camptocamp (http://www.camptocamp.com).
 # @author Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
+from odoo import _, api, exceptions, fields, models
 
 
 class ShopinvaderAddressValidate(models.TransientModel):
@@ -12,12 +12,12 @@ class ShopinvaderAddressValidate(models.TransientModel):
     _name = "shopinvader.address.validate"
     _description = "Shopinvader address validate"
 
-    backend_ids = fields.Many2many(
+    backend_id = fields.Many2one(
         comodel_name="shopinvader.backend",
-        string="ShopInvader Backends",
-        required=True,
+        string="ShopInvader Backend",
+        compute="_compute_valid_backend_ids",
+        store=True,
         readonly=False,
-        default=lambda self: self._default_backend_ids(),
     )
     partner_ids = fields.Many2many(
         string="Addresses",
@@ -25,22 +25,77 @@ class ShopinvaderAddressValidate(models.TransientModel):
         default=lambda self: self._default_partner_ids(),
         required=True,
     )
+    valid_backend_ids = fields.Many2many(
+        comodel_name="shopinvader.backend",
+        compute="_compute_valid_backend_ids",
+    )
+    display_warning = fields.Boolean(
+        compute="_compute_valid_backend_ids",
+    )
     next_state = fields.Selection(
         string="Action",
         selection=[("active", "Activate"), ("inactive", "Inactivate")],
         default="active",
     )
 
-    def _default_backend_ids(self):
-        return self.env.context.get("backend_ids") or self.env[
-            "shopinvader.backend"
-        ].search([])
-
     def _default_partner_ids(self):
         return self.env.context.get("active_ids")
 
+    @api.constrains("partner_ids")
+    def _check_partners(self):
+        for rec in self:
+            if not rec.valid_backend_ids:
+                raise exceptions.UserError(rec._msg_partner_must_have_binding)
+
+    @api.constrains("backend_id")
+    def _check_backend(self):
+        for rec in self:
+            if not rec.backend_id:
+                raise exceptions.UserError(rec._msg_must_select_one_backend)
+            valid = rec._get_valid_backends()
+            if rec.backend_id not in valid:
+                raise exceptions.UserError(rec._msg_must_select_valid_backend)
+
+    @api.depends("partner_ids")
+    def _compute_valid_backend_ids(self):
+        for rec in self:
+            valid = rec._get_valid_backends()
+            rec.valid_backend_ids = valid
+            rec.display_warning = len(valid) > 1
+            if not rec.backend_id and len(valid) == 1:
+                # set default
+                rec.backend_id = valid
+
+    def _get_valid_backends(self):
+        """Get valid backends from partner's hierarchy."""
+        partners = self.partner_ids
+        backends = partners.shopinvader_bind_ids.backend_id
+        while partners.parent_id:
+            backends += partners.parent_id.shopinvader_bind_ids.backend_id
+            partners = partners.parent_id
+        return backends
+
+    @property
+    def _msg_must_select_one_backend(self):
+        return _("You must select a backend")
+
+    @property
+    def _msg_must_select_valid_backend(self):
+        return _("You must select a valid backend. Valid: %s") % ", ".join(
+            self.valid_backend_ids.mapped("name")
+        )
+
+    @property
+    def _msg_partner_must_have_binding(self):
+        return _("Selected partners must have at least a binding")
+
     def action_apply(self):
         self.ensure_one()
+        self._check_backend()
+        self._action_apply()
+        return {"type": "ir.actions.act_window_close"}
+
+    def _action_apply(self):
         active = self.next_state == "active"
         records = self.partner_ids.filtered_domain(
             [("is_shopinvader_active", "!=", active)]
@@ -48,7 +103,7 @@ class ShopinvaderAddressValidate(models.TransientModel):
         records.write({"is_shopinvader_active": active})
         if active:
             for record in records:
-                record._event("on_shopinvader_validate").notify(
-                    record, self.backend_ids
-                )
-        return {"type": "ir.actions.act_window_close"}
+                self._notify_validate("on_shopinvader_validate", record)
+
+    def _notify_validate(self, event_name, record):
+        record._event(event_name).notify(record, self.backend_id)

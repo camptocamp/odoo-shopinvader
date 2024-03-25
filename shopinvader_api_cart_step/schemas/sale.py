0aca158e96e1fea3d394213bd26fd3446fb9300d
Simone Orsi <simahawk@gmail.com>
2024-03-25 10:58:43 +0100

    Add shopinvader_api_cart_step

---
+++ shopinvader_api_cart_step/schemas/sale.py
@@ -0,0 +1,32 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from extendable_pydantic import StrictExtendableBaseModel
+
+from odoo.addons.shopinvader_schema_sale.schemas import Sale as BaseSale
+
+
+class CartStep(StrictExtendableBaseModel):
+    code: str
+    name: str
+
+    @classmethod
+    def from_sale_cart_step(cls, rec):
+        return {"name": rec.name, "code": rec.code} if rec else {}
+
+    @classmethod
+    def from_sale_cart_steps(cls, recs):
+        return [cls.from_sale_cart_step(x) for x in recs]
+
+
+class Sale(BaseSale, extends=True):
+
+    step: CartStep
+    done_steps: list[CartStep] = []
+
+    @classmethod
+    def from_sale_order(cls, odoo_rec):
+        obj = super().from_sale_order(odoo_rec)
+        obj.step = CartStep.from_sale_cart_step(odoo_rec.cart_step_id)
+        obj.done_steps = CartStep.from_sale_cart_steps(odoo_rec.cart_step_done_ids)
+        return obj
